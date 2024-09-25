a276bced0d32c28a132e90b9ca83390585f51d35
AndresJoaquinNino <andresninorodil@gmail.com>
2024-09-24 21:39:40 -0400

    style(functions): change the syntax for generateShortUUID and add types

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,4 +1,4 @@
-export const generateShortUUID = () => {
+export function generateShortUUID(): string{
     return 'xxxxx'.replace(/[x]/g, () => {
         const random = (Math.random() * 16) | 0
         return random.toString(16)
